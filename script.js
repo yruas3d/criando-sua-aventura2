466d9449156518db07332f8b050721c6f1134bb1
yruas3d <yasminruas92@gmail.com>
2024-10-02 13:17:13 -0300

    Primeira vesão do codiógo

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Yasmin Ruas de Jesus 3D 15/09/2024*/
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
